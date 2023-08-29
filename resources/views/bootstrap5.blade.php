745444692df061309556d9efc0b25865bd263def
Dora Otari <doraotari@gmail.com>
2023-08-29 17:11:58 +0700

    pertemuan 4

--- resources/views/bootstrap5.blade.php
+++ resources/views/bootstrap5.blade.php
@@ -41,9 +41,9 @@
                   </ul>
                 </li>
               </ul>
-              <form class="d-flex mt-3" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+              <form class="d-flex mt-3" action="{{ route('logout') }}" method="post">
+                @csrf
+                <button class="btn btn-outline-danger rounded-circle" type="submit"><i class="bi-power"></i></button>
               </form>
             </div>
           </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,22 @@
 @extends('bootstrap5')
 @section('konten')
-    <div class="row">
-        <div class="col">
+    <div class="row justify-content-center mt-5">
+        <div class="col-5">
             <h1 class="display-3"><i class="bi-calendar"></i> Dashboard Presensi</h1>
             <p class="lead">
-
+                Selamat Datang di Dashboard Presensi APP
             </p>
+            <figure>
+                <blockquote class="blockquote">
+                  <p>Patience is the key element of success</p>
+                </blockquote>
+                <figcaption class="blockquote-footer">
+                  Bill Gates 
+                </figcaption>
+            </figure>
         </div>
-        <div class="col">
-            <img src="https://img.freepik.com/free-vector/appointment-booking-smartphone_23-2148559902.jpg?w=740&t=st=1692703109~exp=1692703709~hmac=8a1a8c369ab2126ad308d5d8327aab920572c7b1483380ccadeb88ddf3ccf41e" alt="">
+        <div class="col-4">
+            <img class="img-fluid" src="https://img.freepik.com/free-vector/appointment-booking-smartphone_23-2148559902.jpg?w=740&t=st=1692703109~exp=1692703709~hmac=8a1a8c369ab2126ad308d5d8327aab920572c7b1483380ccadeb88ddf3ccf41e" alt="">
         </div>
     </div>
 @endsection
